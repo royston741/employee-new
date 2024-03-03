d94d72c361cdc5d02da71dba67e6f627ba6f5617
Royston <roy71ston@gmail.com>
2024-03-03 22:15:44 +0530

    chaes in appa

--- src/main/java/com/employee/EmployeeManagementApplication.java
+++ src/main/java/com/employee/EmployeeManagementApplication.java
@@ -16,22 +16,6 @@ import java.util.Optional;
 @Slf4j
 @SpringBootApplication
 public class EmployeeManagementApplication {
-    static List<Employee>  empList=new ArrayList<>();
-
-    public static void details(int maagerId){
-        Employee selectedManager= empList.stream().filter(manager->manager.getId()==maagerId).findFirst().get();
-        System.out.println(selectedManager.getFirstName()+"----------------");
-        empList.forEach(employee -> {
-            try {
-                if (employee.getManager().getId() == maagerId) {
-                    System.out.println(employee.getFirstName());
-                        employee.getTask().forEach(task-> System.out.println("    "+task.getTitle()+"----->"+task.getDescription()));
-                }
-            }catch (Exception e){
-
-            }
-        });
-    }
     public static void main(String[] args) {
         SpringApplication.run(EmployeeManagementApplication.class, args);
     }
